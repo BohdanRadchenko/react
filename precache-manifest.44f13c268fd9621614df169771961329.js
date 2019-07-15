201264b405268e6769ebf8c6fd7c2e2397658976
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-15 10:41:50 +0300

    Updates

---
+++ precache-manifest.44f13c268fd9621614df169771961329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1746416ca97f5a4b71b1a5628f2342",
+    "url": "/react/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "625b095a390eeb5766ef",
+    "url": "/react/goit-react-hw-01-components/static/css/main.18f9d2c0.chunk.css"
+  },
+  {
+    "revision": "c9563366c89b046a3ab0",
+    "url": "/react/goit-react-hw-01-components/static/js/2.b100127d.chunk.js"
+  },
+  {
+    "revision": "625b095a390eeb5766ef",
+    "url": "/react/goit-react-hw-01-components/static/js/main.f3e31cae.chunk.js"
+  },
+  {
+    "revision": "8eef7443f5fe3680cb6c",
+    "url": "/react/goit-react-hw-01-components/static/js/runtime~main.0227b125.js"
+  }
+]);
