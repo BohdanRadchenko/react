e22d413dd2248436df98e6d5f0b7f0e04b642761
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-22 13:15:53 +0300

    complite modal

--- react/module/module-06-07/src/components/ModalDrop/ModalDrop.js
+++ react/module/module-06-07/src/components/ModalDrop/ModalDrop.js
@@ -1,15 +1,46 @@
 /* eskunt-disable */
 /* eslint-disable */
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ModalDrop.module.css';
 
 class ModalDrop extends Component {
   state = {};
+
+  closeRef = createRef();
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleCloseEsc);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleCloseEsc);
+  }
+
+  handleCloseEsc = e => {
+    if (e.key === 'Escape') {
+      const { handleClose } = this.props;
+      handleClose();
+      // document.removeEventListener('keydown', this.handleCloseEsc);
+    }
+  };
+
+  handleCloseClick = e => {
+    const { current } = this.closeRef;
+    if (e.target === current) {
+      const { handleClose } = this.props;
+      handleClose();
+    }
+  };
+
   render() {
     const { handleClose, children } = this.props;
     return (
-      <div className={styles.modalDrops}>
+      <div
+        onClick={this.handleCloseClick}
+        ref={this.closeRef}
+        className={styles.modalDrops}
+      >
         <div className={styles.innerBox}>
           <button onClick={handleClose}>Close</button>
           <p>ModalDrop</p>
